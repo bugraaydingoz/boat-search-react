1086d1895fdca1996f2a5e870c093e428657fc6d
Bugra Aydingoz <bugraaydingoz@gmail.com>
2020-07-19 14:54:49 +0200

    Use real data

--- src/components/boat/Boat.js
+++ src/components/boat/Boat.js
@@ -17,35 +17,70 @@ import {
   DetailText,
 } from './styles'
 
-export function Boat() {
+function sentenceCase(text) {
+  return text.slice(0, 1).toUpperCase() + text.slice(1).toLowerCase()
+}
+
+function getType(type) {
+  return type
+    .split('_')
+    .map((text) => sentenceCase(text))
+    .join(' ')
+}
+
+function truncate(text, length) {
+  if (text.length < length) {
+    return text
+  }
+
+  return text.substr(0, length) + '...'
+}
+
+export function Boat(props) {
+  const {
+    type,
+    name,
+    year,
+    imageUrl,
+    price,
+    reviews,
+    locality,
+    country,
+    cabins,
+    guests,
+    skipper,
+    length,
+  } = props
+  const title = `${getType(type)} ${name} (${year})`
   return (
     <BoatContainer>
-      <Image
-        src="https://d1pkcile4c5gsr.cloudfront.net/boat/0003/81/thumb_280283_boat_expose_414x242.jpeg"
-        alt="Boat picture"
-        lazy
-      />
+      <Image src={imageUrl} alt="Boat picture" lazy />
       <DetailsContainer>
         {/* Title */}
         <Row>
-          <Heading3>Sailboat Superior (1962)</Heading3>
+          <Heading3>{title}</Heading3>
           <Heading3>
-            10€ <Text secondary>/ Day</Text>
+            {price}€ <Text secondary>/ Day</Text>
           </Heading3>
         </Row>
 
         {/* Reviews */}
         <Row>
-          <Reviews score={3} reviews={100} />
+          <Reviews score={reviews.score} reviews={reviews.total} />
           <Text secondary>
-            <Icon name="location" /> Barcelona, Spain
+            <Icon name="location" /> {locality}, {truncate(country, 12)}
           </Text>
         </Row>
 
         {/* Details */}
         <Row>
           <Column>
-            <Details />
+            <Details
+              length={length}
+              cabins={cabins}
+              guests={guests}
+              skipper={skipper}
+            />
           </Column>
           <FlexEnd>
             <Button>Book Now</Button>
@@ -56,24 +91,29 @@ export function Boat() {
   )
 }
 
-function Details() {
+function Details({ cabins, guests, skipper, length }) {
+  let skipperText = skipper.toLowerCase()
+  if (skipperText === 'bareboat') {
+    skipperText = 'not included'
+  }
+
   return (
     <>
       <DetailText>
         <Icon name="boat" />
-        &nbsp; <Text primary>32m&nbsp;</Text> long
+        &nbsp; <Text primary>{length}m&nbsp;</Text> long
       </DetailText>
       <DetailText>
         <Icon name="skipper" />
-        &nbsp; Skipper is <Text primary>&nbsp;included</Text>
+        &nbsp; Skipper is <Text primary>&nbsp;{skipperText}</Text>
       </DetailText>
       <DetailText>
         <Icon name="cabin" />
-        &nbsp; <Text primary>5&nbsp;</Text> cabins
+        &nbsp; <Text primary>{cabins}&nbsp;</Text> cabins
       </DetailText>
       <DetailText>
         <Icon name="guests" />
-        &nbsp; Up to <Text primary>&nbsp;10&nbsp;</Text> guests
+        &nbsp; Up to <Text primary>&nbsp;{guests}&nbsp;</Text> guests
       </DetailText>
     </>
   )
